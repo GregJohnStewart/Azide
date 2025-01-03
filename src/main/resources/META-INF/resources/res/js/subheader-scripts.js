c59ef0e7451f1303ea7443bb54e5137462ccd83e
gpaulin <gregory.j.paulin.civ@army.mil>
2025-01-03 09:33:29 -0500

    First attempt of adding filter box to app bar.

--- src/main/resources/META-INF/resources/res/js/subheader-scripts.js
+++ src/main/resources/META-INF/resources/res/js/subheader-scripts.js
@@ -10,3 +10,30 @@ function toggleAppBar() {
 function loadApp(appId) {
     window.location.href = appId;
 }
+
+
+function filterApps() { 
+    let input = document.getElementById('filterInput').value.toLowerCase(); 
+    let divs = document.querySelectorAll('div.clickable-app'); 
+    console.log("filterApps");
+    console.log(divs.length);
+    divs.forEach(function(div) { 
+        console.log(div.id);
+        if (div.id.toLowerCase().includes(input)) { 
+            div.style.display = 'block'; 
+        } else { 
+            div.style.display = 'none'; 
+        } 
+    });; 
+
+}
+
+function refreshCSS() { 
+    // Select all stylesheets 
+    let stylesheets = document.querySelectorAll('link[rel="stylesheet"]'); 
+    // Iterate over the stylesheets and update the href attribute to force a reload 
+    stylesheets.forEach(function(stylesheet) { 
+        let href = stylesheet.getAttribute('href'); 
+        stylesheet.setAttribute('href', href + '?v=' + new Date().getTime()); 
+    }); 
+}
